6c1d228361b9a79b7db43a31041205776f850322
Aaron Nance <apnance@hotmail.com>
2022-01-19 23:32:44 -0700

    Tweak: optimize initialGuesses using words generated via letter frequency analysis of the World answer pool.

--- Wordler/Configs.swift
+++ Wordler/Configs.swift
@@ -11,17 +11,17 @@ struct Configs {
     
     struct Defaults {
         
-        static var initialGuesses = ["REACT",
-                                    "SIREN",
-                                    "TEARY",
-                                    "ALONE",
-                                    "ARISE",
-                                    "CREAM",
-                                    "ATONE",
-                                    "IRATE",
-                                    "SNARE",
-                                    "WORSE",
-                                    "ANIME"]
+        static var initialGuesses = ["SANER",    // Frequency Score: 4355
+                                     "RENAL",    // Frequency Score: 4405
+                                     "LEARN",    // Frequency Score: 4405
+                                     "ARISE",    // Frequency Score: 4451
+                                     "RAISE",    // Frequency Score: 4451
+                                     "STARE",    // Frequency Score: 4509
+                                     "IRATE",    // Frequency Score: 4511
+                                     "AROSE",    // Frequency Score: 4534
+                                     "ALERT",    // Frequency Score: 4559
+                                     "LATER",    // Frequency Score: 4559
+                                     "ALTER"]    // Frequency Score: 4559
         
         static func randomStarter() -> Word {
             
