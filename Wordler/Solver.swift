9311825dc6d96b70a653e85667d9b725a80e742e
Aaron Nance <apnance@hotmail.com>
2022-01-19 23:35:30 -0700

    Feature/Tidy: 	add Count type alias, remove test code that was moved to unit tests

--- Wordler/Solver.swift
+++ Wordler/Solver.swift
@@ -10,6 +10,7 @@ import Foundation
 typealias Score = Int
 typealias Word = String
 typealias Letter = String
+typealias Count = Int
 
 class Solver {
     
@@ -63,7 +64,7 @@ class Solver {
         for word in _allWords {
             
             assert(word.count == 5, "Word\(word) has invalid length of \(word.count)")
-
+            
         }
         
         return _allWords /*EXIT*/
@@ -126,12 +127,12 @@ class Solver {
             if exclude == "-" { continue /*CONTINUE*/ }
             
             echoMatchCount()
-
+            
             let removeAll = !inclusions.contains(exclude) && !exacts.contains(exclude)
             
             
             for word in wordHopper {
-    
+                
                 let letters = word.map{ String($0) }
                 
                 if removeAll && letters.contains(exclude) {
@@ -246,213 +247,6 @@ class Solver {
             print("Matching Words: \(wordHopper.count)")
             
         }
-    }
-    
-}
-
-// MARK: - Test
-extension Solver {
-    
-    func test() {
-        
-        if !Configs.Test.runSuite { return /*EXIT*/ }
-        
-        func generateAnswerGuess(_ mode: Int) -> [Word] {
-            
-            switch mode {
-                
-                case 0: return ["TREAD", "WORSE"]
-                    
-                case 1:  return ["TORUS", "IRATE"]
-                    
-                case 2:
-                    var answerGuess = [Word]()
-                    
-                for _ in 0..<Configs.Test.runs {
-                        
-                        answerGuess.append(allWords.randomElement!.uppercased())
-                        answerGuess.append(Configs.Defaults.randomStarter().uppercased())
-                        
-                    }
-                    
-                    return answerGuess
-                
-                default: return Configs.Test.data
-                
-            }
-            
-        }
-        
-        let testData = generateAnswerGuess(2)
-        var losers = [Word]()
-        var winners = [Word]()
-        var totalWinningAttempts = 0
-        let totalGames = testData.count / 2
-        
-        var winningInitialGuess = [Word : Int]()
-        var losingInitialGuess  = [Word : Int ]()
-        
-        for word in Configs.Defaults.initialGuesses {
-            
-            winningInitialGuess[word]   = 0
-            losingInitialGuess[word]    = 0
-            
-        }
-        
-        for runNum in 0..<totalGames
-        {
-            
-            resetMatches()
-            
-            let i = runNum * 2
-            let answer = testData[i]
-            var guess = testData[i + 1]
-            
-            let initialGuess = guess
-            
-            var possibleSolutions: (remaining: [Word : Score], suggested: Word) = ([:], "")
-            
-            var attempts = 1
-            
-            repeat {
-                
-                if guess != answer {
-                    
-                    attempts += 1
-                    
-                    let (exc, inc, exa) = analyze(guess: guess, answer: answer)
-                    
-                    possibleSolutions = updateMatches(exclusionsX: exc,
-                                                      inclusionsX: inc,
-                                                      exactsX: exa)
-                    
-                    guess = possibleSolutions.suggested.uppercased()
-                    
-                }
-                
-            } while attempts < 6
-                    && guess != answer
-                    && possibleSolutions.remaining.count > 0
-         
-            // Victory Check
-            let winner = guess == answer
-            var outcome = "WIN"
-            
-            
-            if !winner {
-                
-                losingInitialGuess[initialGuess]! += 1
-                
-                losers.append("\(answer)<\(attempts)>\(initialGuess)")
-                outcome = "LOSS"
-                
-            } else {
-
-                winningInitialGuess[initialGuess]! += 1
-                
-                totalWinningAttempts += attempts
-                winners.append("\(answer)<\(attempts)>\(initialGuess)")
-                
-            }
-            
-            if Configs.Test.echoTestMessages {
-                
-                print("""
-                        
-                            =========================
-                                 \(answer):\(initialGuess)
-                                   - - - - - -
-                               \(outcome) in \(attempts) attempts!
-                            =========================
-                        
-                        """)
-                
-            }
-            
-        }
-        
-        let winningPercentage = ((winners.count.double / totalGames.double) * 100).roundTo(1)
-        let averageAttemptsToWin = (totalWinningAttempts.double / winners.count.double).roundTo(1)
-        
-        var initialGuessResults = ""
-        
-        var initialTab = ""
-        for word in winningInitialGuess.keys {
-            
-            let losses = losingInitialGuess[word]!
-            let wins = winningInitialGuess[word]!
-            let totals = losses + wins
-            let percentWins = wins.percent(of: totals, roundedTo: 1)
-            let percentLoss = losses.percent(of: totals, roundedTo: 1)
-            
-            initialGuessResults += "\(initialTab)\(word) - Win: \(wins) [\(percentWins)]\t Lose: \(losses) [\(percentLoss)]\n"
-            
-            initialTab = "\t"
-        }
-        
-        print("""
-                    
-                    =========================
-                            Results
-                    -------------------------
-                
-                    \tWinners(\(winners.count)):
-                    \t\t\(winners.joined(separator: "\n\t\t\t"))
-                    
-                           - - - - - -
-                    
-                    \tLosers(\(losers.count)):
-                    \t\t\(losers.joined(separator: "\n\t\t\t"))
-                
-                           - - - - - -
-                
-                    Wins:   \(winners.count)/\(totalGames)(\(winningPercentage)%)
-                    Losses: (\(losers.count))
-                
-                    Average Attempts to Win: \(averageAttemptsToWin)
-                
-                    Initial Guess Success Rate:
-                    \(initialGuessResults)
-                    =========================
-                        
-                """)
-        
-    }
-    
-    private func analyze(guess: Word,
-                         answer: Word) -> (excludes: [Letter],
-                                           included: [Letter],
-                                           exacts: [Letter]) {
-        
-        let guess       = guess.uppercased()
-        let answer      = answer.uppercased()
-        
-        let gA          = guess.map{ String($0) } // Guess Array
-        let aA          = answer.map{ String($0) } // Answer Array
-        
-        var excludes    = Array(repeating: "-", count: 5)
-        var includes    = Array(repeating: "-", count: 5)
-        var exacts      = Array(repeating: "-", count: 5)
-
-        for (i, currentLetter) in gA.enumerated() {
-            
-            if currentLetter == aA[i] {
-                
-                exacts[i] = currentLetter
-                
-            } else if aA.contains(currentLetter) {
-                
-                includes[i] = currentLetter
-                
-            } else {
-                
-                excludes[i] = currentLetter
-                
-            }
-            
-        }
-        
-        return (excludes, includes, exacts)
         
     }
     
