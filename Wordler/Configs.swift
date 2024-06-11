595565be584cc6b5a0b1d3ed661a8a2d7bc0e795
Aaron Nance <apnance@hotmail.com>
2024-06-10 21:38:49 -0700

    Update

--- Wordler/Configs.swift
+++ Wordler/Configs.swift
@@ -20,6 +20,56 @@ struct Configs {
             
         }
         
+        struct Console {
+            
+            struct Commands {
+                
+                static var category = "WRDLR"
+                
+                struct Tokens {
+                    
+                    static var last = "last"
+                    static var rem  = "rem"
+                    static var csv  = "csv"
+                    static var del  = "del"
+                    static var add  = "add"
+                    
+                    
+                }
+                
+                struct HelpText {
+                    
+                    static var last = """
+                                        Echoes last chronological answer(s).
+                                        \tUsage:
+                                        \t* 'last' echoes last answer "remembered."
+                                        \t* 'last 5' echoes last 5 answers "remembered."
+                                        
+                                        """
+                    static var rem  = "Echoes all remembered(i.e. answers to past World puzzles) answers to screen."
+                    static var csv  = "Formats remembered answer as CSV and copies to pasteboard."
+                    static var del  = """
+                                        Attemps to delete the specified word from remembered words list.
+                                        \tUsage:
+                                        \t* 'del TRUCK TRIPS ZONKS' attempts to delete words TRUCK
+                                        \t  TRIPS and ZONKS from remembered words.
+                                        \t* 'del 05-23-12' attempts to delete all words added on 05-23-12
+                                        
+                                        """
+                    static var add  = """
+                                        Attemps to add the specified word(s) to remembered words list.
+                                        \tUsage:
+                                        \t* 'add BLANK GONGS TITAN' adds words BLANK, GONGS, and TITAN
+                                        \t  to rememered words list.
+                                        
+                                        """
+                    
+                }
+                
+            }
+            
+        }
+        
         struct File {
             
             static let archivedAnswers      = (name: "Answers",
